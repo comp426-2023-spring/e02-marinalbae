ae0b8f5b67ccc32517ffb677883673bf5654b01a
Eduardo Marin-Alba <emarin-alba@unc.edu>
2023-05-08 16:17:40 -0400

    initial setup

--- server.js
+++ server.js
@@ -62,7 +62,55 @@ const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
-//
+// a04 code
+import { rps, rpsls } from './lib/rpsls.js'
+
+var args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {
+	res.status(200).send('200 OK');
+});
+app.get('/app/rps', (req, res) => {
+	res.status(200).send(JSON.stringify(rps()));
+});
+app.get('/app/rpsls', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls()));
+});
+
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.query.shot)));
+});
+app.get('/app/rpsls/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+});
+
+app.post('/app/rps/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.body.shot)));
+});
+app.post('/app/rpsls/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+});
+
+app.get('/app/rps/play/:shot', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.params.shot)));
+});
+app.get('/app/rpsls/play/:shot', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+});
+
+app.get('*', (req, res) => {
+	res.status(404).send('404 NOT FOUND');
+});
+
+app.listen(port, () => {
+	console.log('Server listening on port ' + port);
+});
+
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
